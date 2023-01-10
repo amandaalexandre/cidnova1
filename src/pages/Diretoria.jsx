27b04403e874dca55449fb4d5fd4075db486a657
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-01-09 23:39:52 -0300

    feat: páginas de diretores

--- src/pages/Diretoria.jsx
+++ src/pages/Diretoria.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { diretoria } from './diretoria'
+import '../assets/styles/Diretoria.css'
+import DiretoriaCard from '../components/DiretoriaCard'
 
 function Diretoria() {
+  const membros = diretoria.map((membro) => {
+      return <DiretoriaCard key={membro.id} {...membro} />
+    })
+  
+
   return (
-    <div>Diretoria</div>
+    <main className='diretoria'>
+      <h1>DIRETORIA</h1>
+
+      <div className='diretoria__grid'>
+      {membros}
+      </div>
+
+    </main>
   )
 }
 
---
+++ src/pages/diretoria.js
@@ -0,0 +1,38 @@
+export const diretoria = [
+    {
+        id: 1,
+        nome: "Jorge Honorato",
+        cargo: "Presidente",
+        imagem: "diretoria/honorato-presidente.jpeg"
+    },
+    {
+        id: 2,
+        nome: "Cabeça Senegal",
+        cargo: "Diretor",
+        imagem: "diretoria/cabeca-senegal-diretor.jpeg"
+    },
+    {
+        id: 3,
+        nome: "Chico Pires",
+        cargo: "Preparador Físico",
+        imagem: "diretoria/chico-pires-preparador-fisico.jpeg"
+    },
+    {
+        id: 4,
+        nome: "Danilo Jansen",
+        cargo: "Treinador",
+        imagem: "diretoria/danilo-jansen-treinador.jpeg"
+    },
+    {
+        id: 5,
+        nome: "Edmilson Reis",
+        cargo: "Administrador",
+        imagem: "diretoria/edmilson-reis-administrador.jpeg"
+    },
+    {
+        id: 6,
+        nome: "Reginaldo Mendes",
+        cargo: "Gerente de Futebol",
+        imagem: "diretoria/reginaldo-mendes-gerente-futebol.jpeg"
+    }
+]
