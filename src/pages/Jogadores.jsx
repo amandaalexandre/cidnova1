2ec01d4ca27726c1aa874bd73d0965566d2f7ba0
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-01-09 23:49:39 -0300

    style: using external file for Players styling

--- src/pages/Jogadores.jsx
+++ src/pages/Jogadores.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../assets/styles/Jogadores.css'
 
 function Jogadores() {
   return (
-    <div style={{color: "white", height: "30vh"}}><h1>EM BREVE!</h1></div>
+    <main className='jogadores'><h1>EM BREVE!</h1></main>
   )
 }
 
