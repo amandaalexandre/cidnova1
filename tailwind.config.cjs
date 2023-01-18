e8b4b443185a80f6ddf46b9f5b0c96d17f061a16
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-01-17 23:41:05 -0300

    style: customize with Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [    
   "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}"],
+  "./src/components/*.{js,jsx}",
+  "./src/pages/*.{js,jsx}"
+],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        escuro: colors.black,
+        vermelho: colors.red,
+        cinza: colors.slate
+      }
+    },
   },
   plugins: [],
 }
