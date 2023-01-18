1b746b468d647c013d572052b7fd6ea992c40305
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-01-18 03:37:01 -0300

    feature: add description component to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Equipe from '../components/Equipe'
 import Hero from '../components/Hero'
 import Siga from '../components/Siga'
+import Descricao from '../components/Descricao'
 
 function Home() {
   return (
     <div>
         <Hero/>
         <Equipe/>
+        <Descricao/>
         <Siga/>
     </div>
   )
