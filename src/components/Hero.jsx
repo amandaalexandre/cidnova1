705516e3243f2e4c61e5b379f13ad1d34f27132c
Amanda Alexandre <amanda.alexandre@outlook.com>
2023-01-17 22:11:14 -0300

    style: setup Tailwind

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import '../assets/styles/Hero.css'
 function Hero() {
   return (
     <div className='Hero'>
-      <h2>ESPORTE CLUBE</h2>
-      <h1>NOVA CIDADE</h1>
+      <h2 className='text-3xl'>ESPORTE CLUBE</h2>
+      <h1 className='text-5xl'>NOVA CIDADE</h1>
     </div>
   )
 }
